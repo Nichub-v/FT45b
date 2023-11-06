0ea0658c4efa4caac91618689d1173def66e6513
nichub <niico.5@live.com.ar>
2023-11-06 17:18:10 -0300

    respuesta M1-01

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,8 +1,30 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let array = num.toString().split("").reverse();
 
-function DecimalABinario(num) {}
+   let suma = 0;
+
+   for (let i = 0; i < array.length; i++) {
+      array[i] = array[i] * 2 ** i;
+      suma = suma + array[i];
+   }
+
+   return suma;
+}
+
+function DecimalABinario(num) {
+     
+   let restos = []
+
+   while (num >= 1) {
+     restos.push(Math.floor(num % 2));
+     num = num / 2;
+   }
+   
+
+   return restos.reverse().join("");
+}
 
 module.exports = {
    BinarioADecimal,
